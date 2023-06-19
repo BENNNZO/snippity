dabc1d5f430e386caa3ac1d9c6e4c2c3083f58bf
Benjamin Phillips <benjamingt537@gmail.com>
2023-06-19 13:32:13 -0400

    .

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -15,6 +15,8 @@ export default function page() {
 
     const [favorites, setFavorites] = useState(false)
 
+    const [snippits, setSnippits] = useState([])
+
     const [user, setUser] = useState(null)
     const [post, setPost] = useState([])
 
@@ -23,12 +25,24 @@ export default function page() {
             .then(res => {
                 setUser(res.data.userData)
                 setPost(res.data.userPost)
+                setSnippits(res.data.userPost)
                 console.log(res.data.userData)
             })
             .catch(err => console.log(err))
     }, [])
 
-    if (!user) {
+    useEffect(() => {
+        setSnippits([])
+        setTimeout(() => {
+            favorites ? (
+                setSnippits(user.favorites)
+            ) : (
+                setSnippits(post)
+            )
+        }, 250);
+    }, [favorites])
+
+    if (!user || !post) {
         return (
             <div className='absolute_center'>
                 <Image 
@@ -45,11 +59,11 @@ export default function page() {
                 <div className='flex flex-row justify-center pt-10'>
                     <h1 className='text-7xl text-red-400 leading-normal inline-block'>{`<`}</h1>
                     <h1 className='text-7xl text-yellow-300 leading-normal inline-block'>{`${session?.user.id === profile ? "MyProfile" : user.username.replace(" ", "")}`}</h1>
-                    <h1 className='text-7xl text-red-400 leading-normal inline-block'>{`/>`}</h1>
+                    <h1 className='text-7xl text-red-400 leading-normal inline-block no-ligs'>{`/>`}</h1>
                 </div>
                 <button className='bg-white px-5 py-2 rounded-md' onClick={() => setFavorites(prev => !prev)}>toggle favorites</button>
                 <div className='mt-10'>
-                    <SnippitLoader size={2} snippits={favorites ? user.favorites : post} setSnippits={setPost} />
+                    <SnippitLoader size={2} snippits={snippits} setSnippits={setSnippits} />
                 </div>
             </div>
         )
